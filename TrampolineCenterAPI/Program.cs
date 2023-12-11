fe7a3ff3ec07d54610d0314e893dfbca9bd805d7
Andrey Starostenkov <st.andrey17@yandex.ru>
2023-12-11 21:45:09 +0300

    some

--- TrampolineCenterAPI/Program.cs
+++ TrampolineCenterAPI/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<APIDbContext>(options => options.UseInMemoryDatabase("ClientsDb"));
 
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
